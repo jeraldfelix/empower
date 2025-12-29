f96f971d99ff515b123558e07bf28cb1f31e7809
Jerald Felix <144522426+jeraldfelix@users.noreply.github.com>
2025-12-29 08:39:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,12 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+export default defineConfig({
+  plugins: [react()],
+  base: "/"
+});
+
+
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
